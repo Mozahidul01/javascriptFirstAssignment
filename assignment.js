e3acbe5079b358de324762a129c21e3965c8e546
mozahidul01 <mozo.postbox@gmail.com>
2020-07-17 18:19:27 +0600

    two function done

---
+++ assignment.js
@@ -0,0 +1,35 @@
+// Convert Feet To Mile
+
+function feetToMile(feet){
+    var mile = feet / 5280;
+    return mile;
+}
+var result = feetToMile(10560);
+console.log(result);
+
+
+// Wood Caltulator
+
+function woodCalculator(chairWood, tableWood, bedWood){
+    var chairWoodCount = chairWood * 1;
+    var tableWoodCount = tableWood * 3;
+    var bedWoodCount = bedWood * 5;
+    var totalWoodCount = chairWoodCount + tableWoodCount + bedWoodCount;
+    return totalWoodCount;
+}
+
+var result = woodCalculator(14, 5, 12);
+console.log(result);
+
+
+// Tiny Friend 
+
+
+
+
+
+
+
+// Brick Calculator
+
+
